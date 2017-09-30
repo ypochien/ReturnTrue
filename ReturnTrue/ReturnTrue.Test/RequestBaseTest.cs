486c30a646a84900c11ca6774aec68428b016738
KirkChen <kirkchen@kkbox.com>
2017-09-30 16:12:26 +0800

    Add GetSignatureSources

---
+++ ReturnTrue/ReturnTrue.Test/RequestBaseTest.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using ReturnTrue.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ReturnTrue.Test
+{
+    public class RequestBaseTest
+    {
+        internal class TestRequest : RequestBase
+        {
+            [JsonProperty("test_field")]
+            public string TestField { get; set; }
+        }
+
+        [Fact]
+        public void TestRequestBaseGetSignatureSources()
+        {
+            // Arrange
+            var expected = new Dictionary<string, string>() {
+                { "test_field", "test" }
+            };
+            Dictionary<string, string> actual;
+            var testRequest = new TestRequest()
+            {
+                TestField = "test"
+            };
+
+            // Act
+            actual = testRequest.GetSignatureSources();
+
+            // Arrange
+            Assert.Equal(expected, actual);
+        }
+    }
+}
