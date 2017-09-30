30bd3d463a9431d303509e47ab3397cc33146073
KirkChen <kirkchen@kkbox.com>
2017-09-30 15:59:41 +0800

    Move model to models directory

--- ReturnTrue/ReturnTrue/Models/BookGrailBookResponse.cs
+++ ReturnTrue/ReturnTrue/Models/BookGrailBookResponse.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ReturnTrue.Services
+namespace ReturnTrue.Models
 {
     public partial class BookGrailBookResponse
     {
