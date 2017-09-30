0ef181cd05b81367034bdc33e42ae117dcb9ec44
strangenaiad <strange.naiad@gmail.com>
2017-09-30 15:49:42 +0800

    unit-test

--- ReturnTrue/Unitest/test_returnTrue.py
+++ ReturnTrue/Unitest/test_returnTrue.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import unittest
 import time
 
@@ -17,13 +18,30 @@ class ReturnTrueTest(unittest.TestCase):
         driver = self.driver
         driver.implicitly_wait(15) 
         driver.get('http://returntrue123456.azurewebsites.net/booking.html')
+        driver.find_element_by_xpath('/html/body/div/div[5]/div/ul/li[1]/div/div/div[1]/select/option[2]').click()
+        time.sleep(1)
+        driver.find_element_by_xpath('/html/body/div/div[5]/div/ul/li[1]/div/div/div[2]/select/option[2]').click()
+        time.sleep(1)
         driver.find_element_by_xpath('//*[@id="tags"]').send_keys('單身')
         time.sleep(1)
-        driver.find_element_by_xpath('/html/body/div/div[2]/div/div/button').click()
-        actual = driver.find_element_by_xpath('//*[@id="swal2-title"]').text
+        driver.find_element_by_xpath('/html/body/div/div[5]/div/div/button').click()
+        try:
+            wait_actual = WebDriverWait(driver, 120).until(
+                EC.presence_of_element_located((By.ID, "swal2-title"))
+            )
+            actual = driver.find_element_by_id("swal2-title").text
+
+        except:
+            driver.quit()
+
         expected = '購票成功'
         self.assertEqual(actual, expected)
         time.sleep(3)
+
+    def tearDown(self):
+        driver = self.driver
+        driver.quit()
+
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
